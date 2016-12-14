9f2bd49ae900d31395512c7b48cb41570dd0ea50
luoy <luoy@emnets.org>
2016-12-14 17:56:07 +0800

    add code

--- cifar.py
+++ cifar.py
@@ -1,5 +1,13 @@
 import os
 import cPickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def rgb_to_gray(img):
+    # img = img_r.astype(np.float32) * 0.2989 + img_b.astype(np.float32) * 0.1140 + img_g.astype(np.float32) * 0.5870
+    # img = img_r * 0.2989 + img_b * 0.1140 + img_g.astype * 0.5870
+    return np.dot(img[...,:3], [0.299, 0.587, 0.114])
 
 
 class CiFar10(object):
@@ -27,7 +35,25 @@ class CiFar10(object):
             for target in self.data_batchs + self.test_batchs:
                 self._load_batch(target)
 
+    def get_test_img(self):
+        return self.raw_data[self.data_batchs[0]]['data'][0]
+
+    def show_test_img(self, img):
+        img = img.reshape(3, 32, 32)
+        plt.subplot(2, 2, 1)
+        plt.imshow(img[0], cmap='gray')
+        plt.subplot(2, 2, 2)
+        plt.imshow(img[1], cmap='gray')
+        plt.subplot(2, 2, 3)
+        plt.imshow(img[2], cmap='gray')
+        plt.subplot(2, 2, 4)
+        img_gray = rgb_to_gray(img.reshape(1024 * 3))
+        plt.imshow(img_gray, cmap='gray')
+        plt.show()
+
 
 if __name__ == '__main__':
     cifar10 = CiFar10()
-    cifar10.load_data('test_batch')
+    cifar10.load_data()
+    img = cifar10.get_test_img()
+    cifar10.show_test_img(img)
